927e168edac0ffaa1f39148cf27c673b68e1988e
Onlynfk <onlygod15@hotmail.com>
2021-05-22 14:11:12 +0100

    handle user token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Login'
 import './App.css'
+import { getTokenFromUrl } from './components/spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+  // useffect runs code base on a given condition
+  useEffect(() => {
+    // code...
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+    }
+    console.log('I have a token  >> 👀', token);
+
+
+  }, []);
+
+
   return (
     <div className="app">
-      <Login/>
+      {token ? <h1> Logged in </h1> :  <Login/> }
+     
     </div>
   )
 }
--- src/components/spotify.js
+++ src/components/spotify.js
@@ -8,7 +8,7 @@ export  const authEndpoint = "https://accounts.spotify.com/authorize/";
 // redirect
 const redirectUri = "http://localhost:3000/";
 
-// my id
+// add your spotify client id
 const clientId = myclientId;
 
 //scopes of spotify ( what you can do with the app)
@@ -21,4 +21,21 @@ const scopes = [
 
 ];
 
+
+// pull my access token
+export const getTokenFromUrl = () =>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) =>{
+        let parts = item.split('=');
+        initial[parts[0]]= decodeURIComponent(parts[1])
+
+        return initial;
+
+    }, {})
+
+}
+
+//http://localhost:3000/#access_token=BQB-5_B5aeNl9KJFZG9lAC6yVbfLOeZXchD6gDxmRivv6DDggPe6T2IEjISTXcU_39qg-QOI0hyxxj9atABg8ZFn1lYN4cH9G_f7WcsxzG_fN_HTkNBWW0KENf0TC8ETwTysp3jakMRtYexgz1FIcwB2I7U5MLkQ9VzBBLWWnszMpRwlGRGC&token_type=Bearer&expires_in=3600
 export const LoginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
